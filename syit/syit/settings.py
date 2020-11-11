1ae37c11864305df1ea60d69f649a0ec2914900b
tsepilovaS <tsepilova00s@mail.ru>
2020-11-12 00:26:42 +0500

    0.0.3

--- syit/syit/settings.py
+++ syit/syit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(r3nt3qw=lh061+$u2t$ksb-f*oty14lg(^s%t_=eni+6n33fy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.34']
+ALLOWED_HOSTS = ['192.168.1.34', '192.168.1.214', '127.0.0.1']
 
 
 # Application definition
